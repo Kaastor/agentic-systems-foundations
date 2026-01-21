4c780b2aa937bc27e21f361e74f280b55aca57ae
przemys <czuba.przemyslaw@gmail.com>
2026-01-21 23:39:16 +0100

    chore: Remove course export and dry run scripts

--- scripts/course_export.sh
+++
@@ -1,8 +0,0 @@
-rsync -a --delete .././var_platform/ .././var_course/ \
-  --exclude 'var/research/' \
-  --exclude 'var/apps/research/' \
-  --exclude 'var/apps/home/' \
-  --exclude 'tests/test_research_*' \
-  --exclude 'tests/test_flaky_verification_and_fault_injection.py' \
-  --exclude 'README_platform.md' \
-  --exclude 'README_research.md'
